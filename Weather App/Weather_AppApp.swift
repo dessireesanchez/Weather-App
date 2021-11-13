704796eb7b1b55ee984580634b316a2184350e70
Adrian De León <adeleon@dlocal.com>
2021-11-12 23:23:14 -0300

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Adrian de Leon on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
