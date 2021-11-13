ac61ca309b40d9caee04cf6845f095a6d574ce6a
Dessiree Sánchez <dessireesanchez@gmail.com>
2021-11-13 00:18:17 -0300

    Create API Struct

---
+++ Weather App/WeatherService.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherService.swift
+//  Weather App
+//
+//  Created by Dessiree Sánchez on 12/11/21.
+//
+import CoreLocation
+import Foundation
+
+struct APIResponse: Decodable {
+    let name: String
+    let main: APIMain
+    let weather: [APIWeather]
+}
+
+struct APIMain: Decodable {
+    let temp: Double
+}
+
+struct APIWeather: Decodable {
+    let description: String
+    let iconName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case description
+        case iconName = "main"
+    }
+}
