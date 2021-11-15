9f6896740391656eaac616f3b33041e57e97e796
Dessiree Sánchez <dessireesanchez@gmail.com>
2021-11-15 00:53:24 -0300

    Complete the Weather Service

--- Weather App/WeatherService.swift
+++ Weather App/WeatherService.swift
@@ -12,6 +12,29 @@ public final class WeatherService: NSObject {
     private let locationManager = CLLocationManager()
     private let API_KEY = "1f4caa563a4545f6291ff64daabd2b2b"
     private var completionHandler: ((Weather)-> Void)?
+    
+    public func loadWeatherData(_ completionHandler: @escaping((Weather) -> Void)) {
+        self.completionHandler = completionHandler
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+    private func makeDataRequest(forCoordinates coordinates: CLLocationCoordinate2D) {
+        
+        guard let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(coordinates.latitude)&lon=\(coordinates.longitude)&appid=\(API_KEY)&units=metric"
+                .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        guard let url = URL(string: urlString) else { return}
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil, let data = data else {return}
+            
+            if let response = try? JSONDecoder().decode(APIResponse.self, from: data) {
+                let weather = Weather(response: response)
+                self.completionHandler?(weather)
+            }
+        } .resume()
+    }
 }
 
 struct APIResponse: Decodable {
